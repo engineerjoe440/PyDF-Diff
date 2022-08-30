8e13b3d864cdb9aec0b7e79d160d742b54a5636c
Joe Stanley <engineerjoe440@yahoo.com>
2022-08-29 22:51:46 -0700

    started basics of diff

--- backend/differ.py
+++ backend/differ.py
@@ -10,6 +10,17 @@ differ.py - the Python difference support file.
 """
 ################################################################################
 
+from pdf_diff.command_line import compute_changes, render_changes
 
+def generate_diff(file_1, file_2, top_margin, bottom_margin, style, width):
+    """Generate Difference between Two PDF Files."""
+    changes = compute_changes(
+        file_1,
+        file_2,
+        top_margin=float(top_margin),
+        bottom_margin=float(bottom_margin)
+    )
+    img = render_changes(changes, style, width)
+    return img
 
 # END
